1a9f5d123d881fe1b385e0d8abe3bb11c97ce6fc
Magesh <tkmagesh77@gmail.com>
2017-12-12 15:48:21 +0530

    spinner with state manager

---
+++ 04-state-manager/stateManager.js
@@ -0,0 +1,42 @@
+let SM = (function(){
+
+	let _reducer = null,
+		_init_action = {type : '@@INIT_STATE'},
+		_currentState = undefined,
+		_subscribers = [];
+
+	function getState(){
+		return _currentState;
+	}
+
+	function subscribe(callback){
+		if (typeof callback === 'function')
+			_subscribers.push(callback);''
+	}
+
+	function triggerChange(){
+		_subscribers.forEach(callback => callback());
+	}
+
+	function dispatch(action){
+		let new_state = _reducer(_currentState, action);
+		if (_currentState === new_state) return;
+		_currentState = new_state;
+		triggerChange();
+	}
+
+	function createStore(reducer){
+		_reducer = reducer;
+		_currentState = reducer(_currentState, _init_action);
+
+		return {
+			getState : getState,
+			subscribe : subscribe,
+			dispatch : dispatch
+		}
+	}
+
+	return {
+		createStore : createStore
+	}
+})();
